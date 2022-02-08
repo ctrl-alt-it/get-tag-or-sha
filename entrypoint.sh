9aa355127aff43a43536d4b422555e28a1c601b6
Aaron C. de Bruyn <aaron@heyaaron.com>
2022-02-08 11:47:52 -0800

    Ignore refs/heads

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env sh
 
+echo $GITHUB_REF
 tag=$(echo $GITHUB_REF | sed 's/refs\/tags\///g')
+echo $tag
 if [ -z "$tag" ]; then
+  tag = $GITHUB_SHA
+fi
+if [[ $tag =~ "refs/heads" ]]; then
   tag = $GITHUB_SHA
 fi
 echo "::set-output name=tag::$tag"
