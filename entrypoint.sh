210a8d2cf068452a3ca42586a0bc1e3d4f09d3f5
Aaron C. de Bruyn <aaron@heyaaron.com>
2022-02-08 11:49:14 -0800

    Var names should really be upper-case

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env sh
 
 echo $GITHUB_REF
-tag=$(echo $GITHUB_REF | sed 's/refs\/tags\///g')
-echo $tag
-if [ -z "$tag" ]; then
-  tag = $GITHUB_SHA
+TAG=$(echo $GITHUB_REF | sed 's/refs\/tags\///g')
+echo $TAG
+if [ -z "$TAG" ]; then
+  TAG=$GITHUB_SHA
 fi
-if [[ $tag =~ "refs/heads" ]]; then
-  tag = $GITHUB_SHA
+if [[ $TAG =~ "refs/heads" ]]; then
+  TAG=$GITHUB_SHA
 fi
-echo "::set-output name=tag::$tag"
+echo "::set-output name=tag::$TAG"
