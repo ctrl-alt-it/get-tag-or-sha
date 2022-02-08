b1206cf826c8e7d72fd580c8ad757152f7957aac
Aaron C. de Bruyn <aaron@heyaaron.com>
2022-02-08 11:57:58 -0800

    Testing

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,11 @@
 #!/usr/bin/env sh
 
 echo $GITHUB_REF
-TAG=$(echo $GITHUB_REF | sed 's/refs\/tags\///g')
-GITTAG=$(git describe --exact-match --tags $(git log -n1 --pretty='%h'))
+TAG=$(git describe --exact-match --tags $(git log -n1 --pretty='%h'))
 echo $(ls -lha)
 echo $GITTAG
 echo $TAG
 if [ -z "$TAG" ]; then
-  TAG=$GITHUB_SHA
-fi
-if [[ $TAG =~ "refs/heads" ]]; then
   TAG=$GITHUB_SHA
 fi
 echo "::set-output name=tag::$TAG"
